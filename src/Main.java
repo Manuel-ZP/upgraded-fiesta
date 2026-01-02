0f588933c925890468f26214dc9236515852cb67
ryoka.wezt@gmail.com <fulgencio.zevallos@usil.pe>
2026-01-01 22:05:42 -0500

    Se agrego mensajes de prueba por consola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hola a todos");
+        System.out.println("Esto es una prueba, por el momento");
 
     }
 }
